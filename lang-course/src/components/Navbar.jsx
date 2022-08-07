51f568b573ca4b0bfbadda2848d18560be1de0a5
Puffy <88508824+PufflyMan@users.noreply.github.com>
2022-08-07 06:40:47 +0400

    nav

---
+++ lang-course/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+export default function() {
+    return (
+        <>
+            <div className="logo">
+                <h1>YuLearn</h1>
+            </div>
+            <div className="nav">
+                <ul>
+                    <li>My Profile</li>
+                    <li>Coins</li>
+                    <li>Free Gifts</li>
+                    <li>Challenges</li>
+                    <li>Premium</li>
+                </ul>
+            </div>
+        </>
+    )
+}
